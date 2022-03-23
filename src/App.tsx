d459de443e9323fdfe3b82a2eb3886a462f2c50d
Bastien TERRIER <bastien.terrier@gmail.com>
2022-03-23 13:06:36 +0100

    feat: support initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
 import BoardContainer from './components/board/container/BoardContainer';
+import { CellProps } from './components/cell/Cell';
+
+const convertInitialStateToCellProps = (input: number[][]): CellProps[][] => {
+  return input.map((row) => row.map((col) => ({ isAlive: !!col })));
+};
+
+const initialState = [
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [1, 1, 1, 1, 1],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+];
 
 const App = () => {
   return (
     <div className="App">
       <h1>Conway's game of life</h1>
 
-      <BoardContainer width={50} height={25} />
+      <BoardContainer
+        width={5}
+        height={5}
+        initialState={convertInitialStateToCellProps(initialState)}
+      />
     </div>
   );
 };
